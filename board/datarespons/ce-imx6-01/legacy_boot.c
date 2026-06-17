ac1b9013858525dc06308d735aaa451ccf9071c2
Mikko Salomaki <mikko.salomaki@akkodis.com>
2026-06-17 15:30:58 +0200

    ce-imx6-01: correctly check partno for image

--- board/datarespons/ce-imx6-01/legacy_boot.c
+++ board/datarespons/ce-imx6-01/legacy_boot.c
@@ -283,17 +283,14 @@ static int load_legacy(const char* interface, int device, int part, const char* 
 	struct disk_partition part_info;
 	int partnr = -1;
 	/* Search by label first, if provided */
-	if (label) {
+	if (label)
 		partnr = part_get_info_by_name(dev, label, &part_info);
-	}
 	/* If not found, search by partition index, if provided */
-	if (partnr == -1 && part != -1) {
-		r = part_get_info(dev, part, &part_info);
-		if (!r) {
+	if (partnr < 1 && part != -1) {
+		if (part_get_info(dev, part, &part_info) == 0)
 			partnr = part;
-		}
 	}
-	if (partnr != -1) {
+	if (partnr > 0) {
 		printf("BOOT: %s %d:%d#\"%s\": %s\n", interface, device, partnr, part_info.name, part_info.uuid);
 	}
 	else {
